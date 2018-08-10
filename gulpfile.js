c9b4707d67846e5c442bf574ef8cab39353b6d99
Andrew Karjagin <42044197+andrewkrajagin@users.noreply.github.com>
2018-08-11 01:53:28 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+﻿const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+  gulp.src('./css/*.*(scss|sass|css)')
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+     }))
+    .pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./css/*.*(scss|sass|css)', ['sass']); 
+})
---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+$('.view-carousel').flickity({
+  cellAlign: 'center',
+  contain: true,
+  imagesLoaded: true,
+  percentPosition: false,
+  wrapAround: false,
+  freeScroll: true
+});
+
+$('.proj-carousel').flickity({
+  cellAlign: 'center',
+  contain: true,
+  imagesLoaded: true,
+  freeScroll: true,
+  prevNextButtons: false
+});
